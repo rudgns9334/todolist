cb157ef61251d7c6be7eb2aca4815923c70a9fd1
rudgns9334 <rudgns9334@gmail.com>
2022-01-08 19:44:34 +0900

    localstorage

--- todolist.js
+++ todolist.js
@@ -1,13 +1,41 @@
 let inputButton = document.querySelector(".button").addEventListener("click",addTODO);
+let clearButton = document.querySelector(".clear").addEventListener("click",clear);
+let key = 0;
+
+window.onload = function(){
+    if(localStorage.length==0){
+        document.querySelector('ul').innerHTML=' ';
+    }
+    else{
+        for(i=0;i<localStorage.length;i++){
+            let value = localStorage.getItem(i);
+            key = i+1;
+            showTODO(value);
+        }
+    }
+
+}
+
+function showTODO(value){
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerHTML = value;
+    ul.appendChild(li);
+    document.querySelector(".list").getElementsByClassName.display = 'block';
+}
 
 function addTODO(e){
     e.preventDefault();
     let todotask = document.querySelector(".todo");
-    let ul = document.querySelector('ul');
-    let li = document.createElement('li');
     let value = todotask.value;
-    li.innerHTML = value;
-    ul.appendChild(li);
-    document.querySelector(".list").getElementsByClassName.display = 'block';
+    localStorage.setItem(key,value);
+    key = key+1;
     todotask.value = '';
+    showTODO(value);
+}
+
+function clear(e){
+    key = 0;
+    localStorage.clear();
+    let ul = document.querySelector('ul').innerHTML=' ';
 }
