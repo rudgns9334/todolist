08e9086d1062cfd41cd11f53c06a2bf23960fc99
rudgns9334 <rudgns8285@naver.com>
2022-01-11 21:25:07 +0900

    select-미완

--- todolist.js
+++ todolist.js
@@ -1,26 +1,26 @@
 let inputButton = document.querySelector(".button").addEventListener("click",addTODO);
 let clearButton = document.querySelector(".clear").addEventListener("click",clear);
-let key = 0;
+let selectDeleteButton = document.querySelector(".s_clear").addEventListener("click",selectDelete);
+let key = 1;
 
 window.onload = function(){
     if(localStorage.length==0){
-        document.querySelector('ul').innerHTML=' ';
+        document.querySelector('ol').innerHTML=' ';
     }
     else{
-        for(i=0;i<localStorage.length;i++){
+        for(i=1;i<=localStorage.length;i++){
             let value = localStorage.getItem(i);
             key = i+1;
             showTODO(value);
         }
     }
-
 }
 
 function showTODO(value){
-    let ul = document.querySelector('ul');
+    let ol = document.querySelector('ol');
     let li = document.createElement('li');
-    li.innerHTML = value;
-    ul.appendChild(li);
+    li.innerHTML = '<label class="check"><input type="checkbox"><label class="value">'+value+'</label><span class="checkmark"></span></label>';
+    ol.appendChild(li);
     document.querySelector(".list").getElementsByClassName.display = 'block';
 }
 
@@ -34,8 +34,35 @@ function addTODO(e){
     showTODO(value);
 }
 
+function selectDelete(e){
+    let ol = document.querySelector('ol');
+    const mycheck = document.getElementsByTagName("li");
+    let leng =mycheck.length;
+    let j = -1;
+    console.log(localStorage[1]);
+    for(i=0;i<leng;i++){
+        j++;
+        if(mycheck[j].firstChild.firstChild.checked){
+            mycheck[j].remove();
+            //localStorage.removeItem(i+1);
+            j=j-1;
+        }
+    }
+}
+/*
+function checkTODO(e){
+    let todo = e.target.nextSibling;
+    if(e.target.checked){
+        todo.style.textDecoration = "line-through";
+    }
+    else{
+        todo.style.textDecoration = "none";
+    }
+}
+*/
+
 function clear(e){
-    key = 0;
+    key = 1;
     localStorage.clear();
-    let ul = document.querySelector('ul').innerHTML=' ';
+    document.querySelector('ol').innerHTML=' ';
 }
